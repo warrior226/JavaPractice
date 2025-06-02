1e73322c1499c74870b036d8d4a47375628f45a0
prayaisse <prayaisse@ticanalyse.org>
2025-06-02 11:17:42 +0000

    Establishing Database Connections in Java:DriverManager vs DataSource

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,49 @@
-import EnumProject.DayOfTheWeek;
-import section21.exo325.Thread1;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
-import java.util.Random;
-
-
-//class OddThread extends Thread{
-//    @Override
-//    public void run() {
-//        for(int i=1;i<=10;i+=2){
-//            System.out.println("OddThread: "+i);
-//            try{
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                System.out.println("OddThread interrupted!");
-//                break;
-//            }
-//        }
-//    }
-//
-//}
-
-class EvenRunnable implements Runnable{
-
-    @Override
-    public void run() {
-        for(int i=2;i<=10;i+=2){
-            System.out.println("EvenThread: "+i);
-            try{
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("EvenThread interrupted!");
-                break;
-            }
-        }
-
-    }
-}
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 public class Main {
+
+    private final static  String CONN_STRING="jdbc:mysql://localhost:3306/music";
     public static void main(String[] args) {
+        String username= JOptionPane.showInputDialog(null,"Enter DB Username");
+        JPasswordField pf = new JPasswordField();
+        int okCxl=JOptionPane.showConfirmDialog(null,pf,"Enter DB Password",JOptionPane.OK_CANCEL_OPTION);
+        final char[] password=(okCxl==JOptionPane.OK_OPTION)?pf.getPassword():null;
+        //Establishing the connection with DriverManager
+//        try {
+//            assert password != null;
+//            try(Connection connection = DriverManager.getConnection(CONN_STRING,username,String.valueOf(password))){
+//                System.out.println("Success !! Connection made to the music database");
+//                Arrays.fill(password,' ');//Here I have reset password by filling it with white space
+//            }
+//        } catch (SQLException e){
+//            throw new RuntimeException(e);
+//        }
 
-        DayOfTheWeek dayOfTheWeek = DayOfTheWeek.FRI;
-        System.out.println(dayOfTheWeek);
-        System.out.printf("The name of the date is %s and the order is %d%n",dayOfTheWeek.name(),dayOfTheWeek.ordinal());
-
-        for(int i=0;i<10;i++){
-            dayOfTheWeek=getRandomDay();
-            //System.out.printf("The name of the date is %s and the order is %d%n",dayOfTheWeek.name(),dayOfTheWeek.ordinal());
-            switchDayOfWeek(dayOfTheWeek);
-
+        //Establishing the connection using DataSource newer than Drivemanager
+        var dataSource=new MysqlDataSource();
+     //   dataSource.setURL(CONN_STRING);
+                //or
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("music");
+        try {
+            assert password != null;
+            try(Connection connection =dataSource.getConnection(username,String.valueOf(password))){
+                System.out.println("Success !! Connection made to the music database");
+                Arrays.fill(password,' ');//Here I have reset password by filling it with white space
+            }
+        } catch (SQLException e){
+            throw new RuntimeException(e);
         }
 
     }
 
-    public static  DayOfTheWeek getRandomDay(){
-        int randomInteger=new Random().nextInt(7);
-        var allDays=DayOfTheWeek.values();
-        return allDays[randomInteger];
-    }
 
-    public static void switchDayOfWeek(DayOfTheWeek weekDay){
-        int weekDayInteger= weekDay.ordinal()+1;
-        switch (weekDay){
-            case MON -> System.out.println("Monday is Day "+ weekDayInteger);
-            case WED ->System.out.println("Wednesday is Day "+ weekDayInteger);
-            default -> System.out.println(weekDay.name().charAt(0)+weekDay.name().substring(1).toLowerCase()+"day is Day "+weekDayInteger);
-        }
-    }
 }
+
