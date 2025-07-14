7628942355f3806a0b73491654843e1fa048945e
prayaisse <prayaisse@ticanalyse.org>
2025-07-14 19:55:43 +0000

    Create and test uri without path

---
+++ src/HttpProject/URIBasics.java
@@ -0,0 +1,40 @@
+package HttpProject;
+
+import java.net.URI;
+
+public class URIBasics {
+
+    public static void main(String[] args) {
+
+        URI timsSite=URI.create("https://learnprogramming.academy/");
+        print(timsSite);
+    }
+
+    private static void print(URI uri){
+        System.out.printf(
+                """
+                        ---------------------------------------------
+                        [scheme:]scheme-specific-part[#fragment]
+                        ---------------------------------------------
+                        Scheme:%s
+                        Scheme-specific part :%s
+                            Authority: %s
+                                User info: %s
+                                Host: %s
+                                Port: %d
+                                Path: %s
+                                Query: %s
+                        Fragment: %s
+                        """,
+                uri.getScheme(),
+                uri.getSchemeSpecificPart(),
+                uri.getAuthority(),
+                uri.getUserInfo(),
+                uri.getHost(),
+                uri.getPort(),
+                uri.getPath(),
+                uri.getQuery(),
+                uri.getFragment()
+        );
+    }
+}
