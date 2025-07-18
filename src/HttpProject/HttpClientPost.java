95104572318af527af832159a1339315c2c73df0
prayaisse <prayaisse@ticanalyse.org>
2025-07-18 16:55:36 +0000

    receiving test.html file from the server

--- src/HttpProject/HttpClientPost.java
+++ src/HttpProject/HttpClientPost.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.util.stream.Stream;
 
@@ -26,14 +27,12 @@ public class HttpClientPost {
                     .uri(URI.create("http://localhost:8080"))
                     .header("Content-Type","application/x-www-form-urlencoded")
                     .build();
-            HttpResponse<Stream<String>> response = httpClient.send(request,HttpResponse.BodyHandlers.ofLines());
+            HttpResponse<Path> response = httpClient.send(request,HttpResponse.BodyHandlers.ofFile(Path.of("test.html"))); //Le server me retourne un fichier
             if(response.statusCode()!=HTTP_OK){
                 System.out.println("Error reading web page "+request.uri());
                 return;
             }
-            response.body().filter(s->s.contains("<h1>"))
-                    .map(s->s.replaceAll("<[^>]*>","").strip())
-                    .forEach(System.out::println);
+
         }catch (IOException  | InterruptedException e){
             throw  new RuntimeException(e);
         }
