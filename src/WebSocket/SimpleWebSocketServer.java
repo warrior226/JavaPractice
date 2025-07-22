d1e6ba30552bb60677b9b6d2a95e7e0f4738db2e
prayaisse <prayaisse@ticanalyse.org>
2025-07-22 01:20:06 +0000

    Build a Java WebSocket Chat Application:Client and Server implementation

--- src/WebSocket/SimpleWebSocketServer.java
+++ src/WebSocket/SimpleWebSocketServer.java
@@ -5,10 +5,14 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleWebSocketServer extends WebSocketServer {
 
     public static  final int SERVER_PORT=8080; //For unencrypt connections and 443 port for encrypt connections
+    private static Map<String,String> map = new HashMap<>();
     public SimpleWebSocketServer() {
         super(new InetSocketAddress(SERVER_PORT));
     }
@@ -19,7 +23,11 @@ public class SimpleWebSocketServer extends WebSocketServer {
     }
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        var resource = webSocket.getResourceDescriptor();
+        String name = resource.split("=")[1];
+        map.put(webSocket.getRemoteSocketAddress().toString(),name);
         System.out.println("Connection openned "+webSocket.getRemoteSocketAddress());
+        broadcastAllButSender(webSocket,"%s joined".formatted(name));
     }
 
     @Override
@@ -30,8 +38,16 @@ public class SimpleWebSocketServer extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket webSocket, String s) {
-        System.out.println("Connection received "+webSocket.getRemoteSocketAddress());
+        //System.out.println("Message  received "+webSocket.getRemoteSocketAddress()+"and data is "+webSocket.getResourceDescriptor());
+        String chatName=map.get(webSocket.getRemoteSocketAddress().toString());
+        broadcastAllButSender(webSocket,"%s: %s".formatted(chatName,s));
 
+    }
+
+    private void broadcastAllButSender(WebSocket webSocket,String message){
+        var connections=new ArrayList<>(getConnections());
+        connections.remove(webSocket);
+        broadcast(message,connections);
     }
 
     @Override
